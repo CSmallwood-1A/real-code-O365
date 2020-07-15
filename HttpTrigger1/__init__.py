74a3194afc18e1c5b62a9472dd9cd118b4e50328
Christopher Smallwood <christopher.smallwood@citrix.com>
2020-07-15 16:43:13 +0100

    trying a pass-through implementation
    Signed-off-by: Christopher Smallwood <christopher.smallwood@citrix.com>

--- HttpTrigger1/__init__.py
+++ HttpTrigger1/__init__.py
@@ -1,24 +1,42 @@
 import logging
-
+import requests
 import azure.functions as func
 
+
+GRAPH_API_URL = 'https://graph.microsoft.com/'
 
 def main(req: func.HttpRequest) -> func.HttpResponse:
     logging.info('Python HTTP trigger function processed a request.')
 
-    name = req.params.get('name')
-    if not name:
-        try:
-            req_body = req.get_json()
-        except ValueError:
-            pass
-        else:
-            name = req_body.get('name')
+    #auth = req.headers['Authorization']
+    split_url = req.url.split('/api/HttpTrigger1/', 1)
+    new_url = GRAPH_API_URL + split_url[1]
+    method = req.method
+    response = requests.request(
+        method=method,
+        url=new_url,
+        headers=req.headers,
+        data=req.get_json()
+    )
+    return func.HttpResponse(f"Hello! Repsonse was{response}")
+    #return func.HttpRequest(method=req.method, url=new_url, headers=req.headers, params=req.params, body=req.body)
 
-    if name:
-        return func.HttpResponse(f"Hello, {name}. Bearer token = {req.headers['Authorization']}")
-    else:
-        return func.HttpResponse(
-             "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.",
-             status_code=200
-        )
+    # pathsplit = url.split('/', 1)
+    
+    # if len(pathsplit) < 2 or pathsplit[0] not in ['v1.0', 'beta']:
+    #     return Response('', status=503)
+    # if request.method == 'GET':
+    #     r = requests.get(f'{GRAPH_API_URL}/{path}', data=request.data,
+    #                      params=request.args, headers=request.headers)
+    # elif request.method == 'POST':
+    #     r = requests.post(f'{GRAPH_API_URL}/{path}', data=request.data, 
+    #                       params=request.args, headers=request.headers)
+    # elif request.method == 'DELETE':
+    #     r = requests.delete(f'{GRAPH_API_URL}/{path}', data=request.data,
+    #                         params=request.args, headers=request.headers)
+    # else:
+    #     raise MiddlewareException("Unhandled forward")
+    # if not r.ok or r.status_code < 200 or r.status_code > 299:
+    #     logging.warning(f"Failed pass-through to {path} with {r.status_code} due to {r.text}")
+    #     logging.warning(request.headers)
+    # return Response(r.content, status=r.status_code)
